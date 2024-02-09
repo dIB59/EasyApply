61416bced33135a31cd738d23c018ab917d442ae
Ibrahim <Ibrahim@ebs.net.pk>
2024-02-09 19:04:52 +0100

    Add event listeners for Apply and Save buttons

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
-console.log("Background script running");
+function logHTML() {
+    const html = document.documentElement.outerHTML;
+    console.log(html);
+}
 
-browser.menus.create({
-    id: "muted-tab",
-    title: "Open in new Muted Tab",
-    contexts: ["link"]
+addEventListener("click", (event) => {
+
+    if (event.target.textContent.includes("Apply")) {
+        console.log("Apply clicked");
+        console.log(event.target);
+    }
+
+    if (event.target.textContent.includes("Save")) {
+        console.log("Save clicked");
+        console.log(event.target);
+    }
+
 });
+
+onclick = (event) => {};
