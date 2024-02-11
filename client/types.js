392d487778466d24334678e1013204734e8e992d
Ibrahim <Ibrahim@ebs.net.pk>
2024-02-11 15:31:14 +0100

    Add LinkedIn job details type definition

---
+++ client/types.js
@@ -0,0 +1,12 @@
+/**
+ * Object to store LinkedIn job details.
+ * @typedef {Object} LinkedinJobDetails
+ * @property {string} allDetails
+ *  - All details extracted from the main content.
+ * @property {string} howYouMatch
+ *  - How you match details extracted from the specific container.
+ * @property {string} jobDetails
+ *  - Job details extracted from the job-details container.
+ * @property {string} companyDetails
+ *  - Company details extracted from the jobs-company__box.
+ */
