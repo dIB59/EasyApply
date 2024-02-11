37821b672d80b30a325ac932371ef09397bd9aa0
Ibrahim <Ibrahim@ebs.net.pk>
2024-02-11 12:33:50 +0100

    Refactor logHTML function and add logging for job details

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
 /** Logs All Text content within the page */
 function logHTML() {
-  const some = document.querySelector('main').textContent.trim();
-  console.log(some);
+  const allDetails = document.querySelector('main')
+      .textContent.replace(/\s+/g, ' ').trim();
+  const jobDetails = document.getElementById('job-details').textContent.trim();
+  console.log(allDetails);
+  console.log(jobDetails);
 }
 
 addEventListener('click', (event) => {
-  console.log(event.target.textContent);
   if (event.target.textContent.includes('Apply')) {
     logHTML();
     console.log('Apply clicked');
   }
-
   if (event.target.textContent.includes('Save')) {
+    logHTML();
     console.log('Save clicked');
   }
 });
-
-onclick = () => {};
