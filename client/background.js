e44951532bc4d07c254cc9cd884f8c10ed7bcdec
Ibrahim <Ibrahim@ebs.net.pk>
2024-02-11 14:40:38 +0100

    Add LinkedIn job details functionality

--- client/background.js
+++ client/background.js
@@ -1,25 +1,11 @@
-/**
- * Gets the job details from the LinkedIn job page
- * when ypou click on the Apply or Save button
- * @return {string} The job details
- */
-function getLinkdenJobDetails() {
-  const allDetails = document.querySelector('main')
-      ?.textContent.replace(/\s+/g, ' ')
-      ?.trim();
-  const jobDetails = document.getElementById('job-details')
-      ?.textContent
-      ?.trim();
-  console.log(allDetails);
+import {getLinkedinJobDetails} from './linkedin';
 
-  return jobDetails ?? 'No Job details found';
-}
 
 addEventListener('click', (event) => {
   if (event.target.textContent.includes('Apply')) {
-    console.log(getLinkdenJobDetails());
+    console.log(getLinkedinJobDetails());
   }
   if (event.target.textContent.includes('Save')) {
-    console.log(getLinkdenJobDetails());
+    console.log(getLinkedinJobDetails());
   }
 });
